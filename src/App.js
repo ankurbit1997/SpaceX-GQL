16319e431e351708c6cc9c601960368bc41b4272
ankurbit1997 <31044900+ankurbit1997@users.noreply.github.com>
2021-09-06 15:57:23 +0530

    added sass and removed unused files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import "./App.css";
+import React, { useState } from "react";
+import "./app.scss";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <h1>hello world from branch</h1>
+      <h1>hello world {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
     </div>
   );
-}
+};
 
 export default App;

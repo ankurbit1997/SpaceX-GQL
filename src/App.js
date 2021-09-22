9c19e8734f860b0a410c5e9d877fc24e77ea751e
ankurbit1997 <31044900+ankurbit1997@users.noreply.github.com>
2021-09-22 19:00:52 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import "./app.scss";
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Launches from "./pages/Launches";
 const App = () => {
-  
   return (
     <div>
-      <h1>hello</h1>
+      <h1>Header</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/launches" component={Launches} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
+      <h1>Footer</h1>
     </div>
   );
 };
---
+++ src/pages/Launches.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Launches = () => {
+  return (
+    <div>
+      <h1>launches</h1>
+    </div>
+  );
+};
+
+export default Launches;

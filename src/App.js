4b2d2e4b8c89b75e0dc3ffcf1ca5c5a17ac7aaa0
ankurbit1997 <31044900+ankurbit1997@users.noreply.github.com>
2021-09-22 18:44:09 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
 import "./app.scss";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  
   return (
-    <div className="App">
-      <h1>hello world {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>Increase</button>
+    <div>
+      <h1>hello</h1>
     </div>
   );
 };
